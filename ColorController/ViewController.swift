17e5f5260fc6d221a53863d5f5e3a2984a359be5
Konstantin <diel404@gmail.com>
2023-11-19 20:39:51 -0800

    Do app ColorController

--- ColorController/ViewController.swift
+++ ColorController/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var resultColorView: UIView!
+    
+    @IBOutlet var redNameLabel: UILabel!
+    @IBOutlet var greenNameLabel: UILabel!
+    @IBOutlet var blueNameLabel: UILabel!
+    
+    @IBOutlet var redPointLabel: UILabel!
+    @IBOutlet var greenPointLabel: UILabel!
+    @IBOutlet var bluePointLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resultColorView.layer.cornerRadius = 10
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
+        updateResultColorView()
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        redPointLabel.text = String(format: "%.2f", redSlider.value)
+        updateResultColorView()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenPointLabel.text = String(format: "%.2f", greenSlider.value)
+        updateResultColorView()
+    }
+    
+    @IBAction func blueSliderAction() {
+        bluePointLabel.text = String(format: "%.2f", blueSlider.value)
+        updateResultColorView()
+    }
+    
+    private func updateResultColorView() {
+        let redColor = CGFloat(redSlider.value)
+        let greenColor = CGFloat(greenSlider.value)
+        let blueColor = CGFloat(blueSlider.value)
+        
+        resultColorView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1.0)
+    }
 }
 
